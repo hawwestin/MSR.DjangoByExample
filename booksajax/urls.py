17edd294c97a9632b0470435956e88688f2110c9
hawwestin <michal.robaszewski+git@gmail.com>
2019-02-24 23:10:22 +0100

    add books js

--- booksajax/urls.py
+++ booksajax/urls.py
@@ -20,4 +20,5 @@ from crudbook import views
 urlpatterns = [
     path('books/', views.book_list, name='book_list'),
     path('admin/', admin.site.urls),
+    path('books/create/', views.book_create, name='book_create'),
 ]
--- crudbook/views.py
+++ crudbook/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 from .models import Book
+from .forms import BookForm
 
 
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'crudbook/book_list.html', {'books': books})
+
+
+def book_create(request):
+    form = BookForm()
+    context = {'form': form}
+    html_form = render_to_string('books/includes/partial_book_create.html',
+                                 context,
+                                 request=request
+                                 )
+    return JsonResponse({'html_form': html_form})
