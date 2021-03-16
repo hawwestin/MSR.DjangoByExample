414f4d0126aaec288ebe8ff7cad9be802c341a30
hawwestin <michal.robaszewski+git@gmail.com>
2021-03-16 20:38:58 +0100

    Fail over protection for not provided env google variables

--- zSPA/zSPA/spa/views.py
+++ zSPA/zSPA/spa/views.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render
 from decouple import config
 
-
 from .models import Images
+
+
 # Create your views here.
 
 def index(request, **kwargs):
-	"""
-	Render single page application and provide context data.
-	:param request:
-	:param kwargs:
-	:return:
-	"""
-	context = {
-		'images': Images.objects.filter(is_active=True),
-		'googleForm': config('googleForm'),
-		'googleMaps': config('googleMaps'),
-	}
+    """
+    Render single page application and provide context data.
+    :param request:
+    :param kwargs:
+    :return:
+    """
+    context = {
+        'images': Images.objects.filter(is_active=True),
+        'googleForm': config('googleForm', default=''),
+        'googleMaps': config('googleMaps', default=''),
+    }
 
-	return render(request, 'spa/main_SPA.html', context)
-
-
-
-
+    return render(request, 'spa/main_SPA.html', context)
